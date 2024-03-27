90e36a208e12e11923cdc83bcb5a32d955aeb5ee
Héloïse Rigaux <heloise.rigaux@etu.umontpellier.fr>
2024-03-27 15:48:10 +0100

    Modification Cartes

--- src/main/java/fr/umontpellier/iut/trains/cartes/Cooperation.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Cooperation.java
@@ -2,6 +2,6 @@ package fr.umontpellier.iut.trains.cartes;
 
 public class Cooperation extends Rail {
     public Cooperation() {
-        super("Coopération", 5, 0, "Piochez une carte ferraille. Aucun surcout pour poser des rails là où vos adversaires ont des rails. Vous ne recevez pas de cartes ferraille");
+        super("Coopération", 5, "Piochez une carte ferraille. Aucun surcout pour poser des rails là où vos adversaires ont des rails. Vous ne recevez pas de cartes ferraille");
     }
 }
