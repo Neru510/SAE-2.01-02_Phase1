4f39af02c8addfde7ed57084c92c9af824d4d46b
Héloïse Rigaux <heloise.rigaux@etu.umontpellier.fr>
2024-04-21 19:03:38 +0200

    Ajout jouabilité TGV

--- src/main/java/fr/umontpellier/iut/trains/cartes/TGV.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/TGV.java
@@ -1,7 +1,19 @@
 package fr.umontpellier.iut.trains.cartes;
+import fr.umontpellier.iut.trains.Joueur;
 
 public class TGV extends TrainAction {
     public TGV() {
-        super("TGV",2, 1, "Recevez une pièce si vous avez un Train omnibus en jeu");
+        super("TGV",2, 1);
+    }
+
+    @Override
+    public void jouer(Joueur joueur){
+        super.jouer(joueur);
+        for (Carte c : joueur.getCartesEnJeu()){
+            if (c.getNom().equals("Train omnibus")){
+                joueur.ajouterArgent(1);
+                break;
+            }
+        }
     }
 }
