f3476ccba8d7013b0db63d4102ce12fa0b201a85
Héloïse Rigaux <heloise.rigaux@etu.umontpellier.fr>
2024-04-21 18:53:19 +0200

    Ajout jouabilité FeuDeSignalisation

--- src/main/java/fr/umontpellier/iut/trains/cartes/Carte.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Carte.java
@@ -1,6 +1,9 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Bouton;
 import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.*;
 
 public abstract class Carte {
     private final String nom;
@@ -108,5 +111,136 @@ public abstract class Carte {
 
     public void setFeraille(boolean ferraille){
         this.ferraille=ferraille;
+    }
+
+    public static List<String> choixOuiNon(){
+        List<String> choixPossibles = new ArrayList<>();
+        choixPossibles.add("oui");
+        choixPossibles.add("non");
+        return choixPossibles;
+    }
+
+    public static List<Bouton> choixOuiNonBouton(){
+        List<Bouton> boutonList = new List<>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<Bouton> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(Bouton bouton) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Bouton> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, Collection<? extends Bouton> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Bouton get(int index) {
+                return null;
+            }
+
+            @Override
+            public Bouton set(int index, Bouton element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, Bouton element) {
+
+            }
+
+            @Override
+            public Bouton remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public ListIterator<Bouton> listIterator() {
+                return null;
+            }
+
+            @Override
+            public ListIterator<Bouton> listIterator(int index) {
+                return null;
+            }
+
+            @Override
+            public List<Bouton> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
+        Bouton oui = new Bouton("oui", "oui");
+        Bouton non = new Bouton("non", "non");
+        boutonList.add(oui);
+        boutonList.add(non);
+        return boutonList;
     }
 }
--- src/main/java/fr/umontpellier/iut/trains/cartes/FeuDeSignalisation.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/FeuDeSignalisation.java
@@ -1,7 +1,26 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+import fr.umontpellier.iut.trains.Bouton;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FeuDeSignalisation extends Action {
     public FeuDeSignalisation() {
-        super("Feu de signalisation", 2, 0, "Piochez 1 carte puis consultez la première carte de votre deck. Défaussez-la ou replacez-la sur le dessus de votre deck.");
+        super("Feu de signalisation", 2, 0);
+    }
+
+    @Override
+    public void jouer(Joueur joueur){
+        super.jouer(joueur);
+        joueur.piocherEtAjouterMain();
+        List<String> choixPossibles = choixOuiNon();
+        List<Bouton> boutonList = choixOuiNonBouton();
+        String choix = joueur.choisir("Consultez la première carte de votre deck. Défaussez-la ou replacez-la sur le dessus de votre deck.", choixPossibles, null, false);
+        if (choix.equals("oui")){
+            Carte c = joueur.piocher();
+            joueur.defausser(c);
+        }
     }
 }
