76bd7d61d1d3f31a908c18a544c987baef2b22f3
Lorenzo Eden <eden.lorenzo@etu.umontpellier.fr>
2024-04-25 15:59:36 +0200

    HorairesTemporaires fait

--- src/main/java/fr/umontpellier/iut/trains/cartes/HorairesTemporaires.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/HorairesTemporaires.java
@@ -1,7 +1,30 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Bouton;
+import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HorairesTemporaires extends Action {
     public HorairesTemporaires() {
-        super("Horaires temporaires", 5, 0, "Dévoilez des cartes de votre deck jusqu'à ce que vous dévoiliez 2 trains. Ajoutez ces cartes train à votre main et défaussez le reste.");
+        super("Horaires temporaires", 5, 0);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        super.jouer(joueur);
+        int i = 0;
+        while (i == 2 || joueur.getPioche().isEmpty()) {
+            Carte carte = joueur.piocher();
+            joueur.devoilerCarte(carte);
+            if (carte.getType().equals("Train")){
+                i++;
+            }
+            else {
+                joueur.removeCarte(carte);
+                joueur.defausser(carte);
+            }
+        }
     }
 }
